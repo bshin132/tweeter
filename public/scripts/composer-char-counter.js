a296c97f3c5af30a34ebb1bb4360d67f9b8c8931
Brian <bshin132@gmail.com>
2021-12-15 03:30:50 +0000

    implemented counter function

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  //create a listener for textarea value
+  $("#tweet-text").on("input", function(event) {
+    //get the value of text input
+    const $numberOfValues = $(this).val().length;
+    //do the calculation to countdown from 140
+    const $mathValues = 140 - $numberOfValues;
+    //change the html value to the assigned value
+
+    $($(this)).siblings(".tweet-count").find(".counter").html($mathValues);
+
+    
+  
+
+    //change the color of texts if below 0
+    if ($mathValues < 0) {
+      $(".counter").css({
+        'color': '#FF0000',
+      })
+    } else {
+      $(".counter").css({
+        'color': '#535149',
+      })
+    }
+  })
+});
